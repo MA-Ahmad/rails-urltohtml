35fcbdfe1d0ec41a19e7f9c4f87ea3af29e21bb5
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-03-15 04:09:11 -0700

    logic for url to html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
+
   def index
+    if params[:page] && params[:page][:url].present?
+      begin
+        @doc = Nokogiri::HTML(URI.open(params[:page][:url]).read)
+        @summary = @doc.xpath("//*").map(&:name).each_with_object({}) {|n, r| r[n] = (r[n] || 0) + 1 }
+        @error = false
+      rescue Exception => e
+        puts "Couldn't read \"#{ params[:page][:url] }\": #{ e }"
+        @error = true
+      end
+    end
   end
+
 end
